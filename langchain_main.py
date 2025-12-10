0303d287e14d3476fe23f955d558b19adfeb12c6
Smarak Patra <43983935+LeoParadox10@users.noreply.github.com>
2025-12-10 17:44:50 +0530

    Change GOOGLE_API_KEY in get_few_shots_db_chain
    Updated GOOGLE_API_KEY in environment variable setup.

--- langchain_main.py
+++ langchain_main.py
@@ -19,7 +19,7 @@ load_dotenv()
 def get_few_shots_db_chain():
     if "GOOGLE_API_KEY" not in os.environ:
         os.environ["GOOGLE_API_KEY"] = getpass.getpass(
-            "AIzaSyA27S0zKnqOnxlwCCgr_MLm1W86w_f7iOc")
+            "AIzaSyA4mY144wkv4NHuftmo59aDADDUxug-LeQ")
     llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.2)
 
     db_user = 'root'
